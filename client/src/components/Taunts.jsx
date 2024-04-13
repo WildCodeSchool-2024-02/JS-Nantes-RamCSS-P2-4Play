bbbf6f286bc59a825a51e36b0a5f4460fef38f49
brewost <156411525+brewost@users.noreply.github.com>
2024-04-13 13:08:57 +0200

    installed react simple keyboard and set up KeyboardContainer component

--- client/src/components/Taunts.jsx
+++ client/src/components/Taunts.jsx
@@ -1,7 +1,12 @@
+import KeyboardContainer from "./keyboard/KeyboardContainer";
+
 function Taunts() {
-    return (
-    <h1>Salut c'est moi Taunts</h1>
-    );
+  return (
+    <>
+      <h1>Salut c'est moi Taunts</h1>
+      <KeyboardContainer />
+    </>
+  );
 }
 
-export default Taunts; 
+export default Taunts;
---
+++ client/src/components/keyboard/KeyboardContainer.jsx
@@ -0,0 +1,80 @@
+import { useRef, useState } from "react";
+import { KeyboardReact as Keyboard } from "react-simple-keyboard";
+
+// DEPS: npm install react-simple-keyboard --save
+// You just need KeyboardWrapper.jsx and keyboard-styles.css in a folder
+
+// Mobile layout theme demo here : https://hodgef.com/simple-keyboard/editor/?d=hodgef/simple-keyboard-npm-demos/tree/mobile
+// NB a lot of themes / demos use vanilla JS; you need to find the options used when a New Keyboard class call is made and then
+// use those as props to pass to the <Keyboard /> component
+
+// Docs for the library will all the possible options: https://hodgef.com/simple-keyboard/documentation/options/
+
+import "react-simple-keyboard/build/css/index.css";
+import "./KeyboardContainer.css";
+
+function KeyboardContainer() {
+  const [input, setInput] = useState("");
+  const [layout, setLayout] = useState("default");
+  const keyboard = useRef();
+
+  const onChange = (changeInput) => {
+    setInput(changeInput);
+    // console.log("Input changed", input);
+  };
+
+  const handleShift = () => {
+    const newLayoutName = layout === "default" ? "shift" : "default";
+    setLayout(newLayoutName);
+  };
+
+  const onKeyPress = (button) => {
+    // console.log("Button pressed", button);
+
+    /**
+     * If you want to handle the shift and caps lock buttons
+     */
+    if (button === "{shift}" || button === "{lock}") handleShift();
+  };
+
+  const onChangeInput = (event) => {
+    const newInput = event.target.value;
+    setInput(newInput);
+    keyboard.current.setInput(newInput);
+  };
+
+  return (
+    <div>
+      <input
+        value={input}
+        // placeholder //
+        onChange={onChangeInput}
+      />
+
+      <Keyboard
+        // eslint-disable-next-line no-return-assign
+        keyboardRef={(r) => (keyboard.current = r)}
+        layoutName={layout}
+        onChange={onChange}
+        onKeyPress={onKeyPress}
+        layout={{
+          default: [
+            "q w e r t y u i o p",
+            "a s d f g h j k l",
+            "{shift} z x c v b n m {backspace}",
+            "{numbers} {space} {ent}",
+          ],
+          shift: [
+            "Q W E R T Y U I O P",
+            "A S D F G H J K L",
+            "{shift} Z X C V B N M {backspace}",
+            "{numbers} {space} {ent}",
+          ],
+          numbers: ["1 2 3", "4 5 6", "7 8 9", "{abc} 0 {backspace}"],
+        }}
+      />
+    </div>
+  );
+}
+
+export default KeyboardContainer;
