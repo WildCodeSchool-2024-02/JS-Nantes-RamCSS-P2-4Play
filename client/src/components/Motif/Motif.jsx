17243d6e987a46f4e8d66024bbe3924f181296c0
Florine <florinevinet@gmail.com>
2024-04-17 16:46:33 +0200

    API fetched and solution shown in the game

--- client/src/components/Motif/Motif.jsx
+++ client/src/components/Motif/Motif.jsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import KeyboardContainer from "../keyboard/KeyboardContainer";
 
 function Motif() {
   const [input, setInput] = useState("");
+  const [solution, setSolution] = useState(null);
+
+  useEffect(() => {
+    fetch("https://my-json-server.typicode.com/florine-vnt/words-api/coiffeurs")
+      .then((response) => response.json())
+      .then((data) => {
+        // random int between 0 & 29 
+        const randomSolution = data[Math.floor(Math.random() * data.length)];
+        setSolution(randomSolution.nom);
+      });
+  }, [setSolution]);
+
   return (
     <>
-    <h1>Salut c'est moi Motif</h1>
-   <KeyboardContainer input={input} setInput={setInput} />
+      <h1>Salut c'est moi Motif</h1>
+      {solution && <div>Solution is {solution}</div>}
+      <KeyboardContainer input={input} setInput={setInput} />
     </>
   );
 }
