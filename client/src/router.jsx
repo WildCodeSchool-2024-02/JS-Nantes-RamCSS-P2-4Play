090d06f69c5c5c05c30e4a016450e8dd7ff10169
NICOLAS MICHEL <nicolas_m_44@hotmail.fr>
2024-04-19 14:41:46 +0200

    ✅ US-16 : Réparation du router

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import HomePage from "./components/HomePage/HomePage";
-import Hangcheese from "./components/Hangcheese/Hangcheese";
+import HangCheese from "./components/Hangcheese/HangCheese";
 import Taunts from "./components/Taunts/Taunts";
 import Motif from "./components/Motif/Motif";
 import Synaps from "./components/Synaps/Synaps";
@@ -18,7 +18,7 @@ const router = createBrowserRouter([
       },
       {
         path: "/hangcheese",
-        element: <Hangcheese />,
+        element: <HangCheese />,
       },
       {
         path: "/taunts",
