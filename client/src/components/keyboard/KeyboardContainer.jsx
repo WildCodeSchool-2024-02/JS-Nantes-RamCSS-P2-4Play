2ccfcac4ee7970da96ec556040d4ea55bfb589b3
Florine <florinevinet@gmail.com>
2024-04-21 18:13:15 +0200

    :construction: shift and numbers touches removed from keyboard + working on a max length authorized

--- client/src/components/keyboard/KeyboardContainer.jsx
+++ client/src/components/keyboard/KeyboardContainer.jsx
@@ -65,8 +65,8 @@ function KeyboardContainer({ input, setInput }) {
           default: [
             "Q W E R T Y U I O P",
             "A S D F G H J K L",
-            "{shift} Z X C V B N M {backspace}",
-            "{numbers} {space} {ent}",
+            "Z X C V B N M {backspace}",
+            "{space} {ent}",
           ],
           shift: [
             "q w e r t y u i o p",
