46805f48a4295eceaa67ce503b5147879b9c918a
Florine <florinevinet@gmail.com>
2024-04-30 14:06:11 +0200

    url change on hangcheese

--- client/src/components/Hangcheese/EndMessage.jsx
+++ client/src/components/Hangcheese/EndMessage.jsx
@@ -30,7 +30,7 @@ function EndMessage({ endMessage, solution }) {
       )}
 
       <div className="motif-solution">La solution était {solution}</div>
-      <a href="http://localhost:3000/hangcheese">
+      <a href={`${import.meta.env.VITE_API_URL}/hangcheese`}>
         <button className="motif-button" type="button">
           RESET
         </button>
